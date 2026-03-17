530d1c4d337efd55f17316705e2caf1abbb8283d
jib3v3 <jeanbaptiste.voillemin@gmail.com>
2026-03-17 16:58:35 +0100

    Modification annulée

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -28,13 +28,13 @@ namespace Calculs
                 Console.Write("Choix :                          ");
                 choix = Console.ReadLine();
 
-                val1 = rand.Next(1, 10);
-                val2 = rand.Next(1, 10);
+             
                 // traitement des choix
                 switch (choix)
                 {
                     case "1": // addition
-                        
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
                         // saisie de la réponse
                         correct = false;
                         while (!correct)
@@ -62,7 +62,8 @@ namespace Calculs
                         }
                         break;
                     case "2": // multiplication
-                        
+                        val1 = rand.Next(1, 10);
+                        val2 = rand.Next(1, 10);
                         // saisie de la réponse
                         correct = false;
                         while (!correct)
